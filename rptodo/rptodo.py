ad5c78d0bc0f7b428ec9bbd0259e3dd3fce7f701
Sarthak <sarthaktomar2143@gmail.com>
2022-08-01 15:39:48 +0000

    Added the Controller class, Todoer.

--- rptodo/rptodo.py
+++ rptodo/rptodo.py
@@ -1,9 +1,14 @@
 """
 This module provides our app Model-Controller.
 """
-
+from pathlib import Payh
 from typing import Any, Dict, NamedTuple
+from rptodo.database import DataBaseHandler
 
 class CurrentTodo(NamedTuple):
     todo: Dict[str, Any]
     error: int
+
+class Todoer:
+    def __init__(self, db.path: Path) -> None:
+        self._db_handler = DataBaseHandler(db_path)
