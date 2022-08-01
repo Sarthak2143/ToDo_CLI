d3a106bb9e7a2f53e78791480f1b233666bb64ef
Sarthak <sarthaktomar2143@gmail.com>
2022-08-01 15:55:12 +0000

    Added a pytest fixture for adding to-do's and added some test cases to test against it.

--- tests/test_rptodo.py
+++ tests/test_rptodo.py
@@ -1,5 +1,12 @@
 from typer.testing import CliRunner
-from rptodo import __app_name__, __version__, cli
+from rptodo import (
+    __app_name__,
+    __version__,
+    DB_READ_ERROR,
+    SUCCESS,
+    cli,
+    rptodo,
+)
 
 runner = CliRunner()
 
@@ -7,3 +14,40 @@ def test_version():
     result = runner.invoke(cli.app, ["--version"])
     assert result.exit_code == 0
     assert f"{__app_name__} v{__version__}\n" in result.stdout
+
+
+"""
+To test .add(), we must create a Todoer instance with a proper JSON file as the target database. To provide that file, we’ll use a pytest fixture.
+"""
+
+@pytest.fixture
+def mock_json_file(tmp_path):
+    todo = [{"Description": "Work on flexbox", "Priority": 2, "Done": False}]
+    db_file = tmp_path / "todo.json"
+    with db_file.open("w") as db:
+        json.dump(todo, db, indent=4)
+    return db_file
+
+## Tests
+
+test_data1 = {
+    "description": ["Make", "a", "game"],
+    "priority": 1,
+    "todo": {
+        "Description": "Make a game.",
+        "Priority": 1,
+        "Done": False,
+    },
+}
+
+test_data2 = {
+    "description": ["Wash", "the", "car"],
+    "priority": 2,
+    "todo": {
+        "Description": "Wash the car.",
+        "Priority": 2,
+        "Done": False,
+    },
+}
+
+
